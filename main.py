e2b378b3946f2d02aaa47d03f4945f8caaad4f87
Koscee <52253099+Koscee@users.noreply.github.com>
2023-09-03 00:51:01 +0800

    feat: add check marks, implement timer reset, and disable start button during count down

--- main.py
+++ main.py
@@ -2,20 +2,34 @@ import math
 from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
+DEFAULT_CAPTION = "Timer"
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer_id = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    window.after_cancel(timer_id)
+    canvas.itemconfig(timer_text, text="00:00")
+    caption_label.config(text=DEFAULT_CAPTION, fg=GREEN)
+    check_mark.config(text="")
+    start_btn.config(state="active")
+    global reps
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
+    start_btn.config(state="disabled")
+
     global reps
     reps += 1
 
@@ -43,9 +57,13 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer_id
+        timer_id = window.after(1000, count_down, count - 1)
     else:
         start_timer()
+        # add check mark every time a work session is completed
+        if reps % 2 == 0:
+            check_mark.config(text=f"{check_mark['text']}✔")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -53,7 +71,7 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-caption_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50, "bold"))
+caption_label = Label(text=DEFAULT_CAPTION, bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50, "bold"))
 caption_label.grid(row=0, column=1)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
@@ -65,10 +83,10 @@ canvas.grid(row=1, column=1)
 start_btn = Button(text="Start", font=("", 13), padx=6, pady=3, command=start_timer)
 start_btn.grid(row=2, column=0)
 
-reset_btn = Button(text="Reset",  font=("", 13), padx=6, pady=3)
+reset_btn = Button(text="Reset",  font=("", 13), padx=6, pady=3, command=reset_timer)
 reset_btn.grid(row=2, column=2)
 
-check_mark = Label(text="✔",  bg=YELLOW, fg=GREEN, font=(FONT_NAME, 14, "bold"))
+check_mark = Label(bg=YELLOW, fg=GREEN, font=(FONT_NAME, 14, "bold"))
 check_mark.grid(row=3, column=1)
 
 
