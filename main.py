850c7ab08e62d652a6209d354c0f70453faa98bb
Koscee <52253099+Koscee@users.noreply.github.com>
2023-09-02 23:57:03 +0800

    feat: set different timer sessions and change caption label dynamically

--- main.py
+++ main.py
@@ -7,19 +7,36 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps == 8:
+        count_down(long_break_sec)
+        caption_label.config(text="BREAK", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        caption_label.config(text="BREAK", fg=PINK)
+    else:
+        count_down(work_sec)
+        caption_label.config(text="WORK", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
+    global reps
     count_min = math.floor(count / 60)
     count_sec = count % 60
     count_sec = f"0{count_sec}" if count_sec < 10 else count_sec
@@ -27,6 +44,8 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -34,8 +53,8 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50))
-timer_label.grid(row=0, column=1)
+caption_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50, "bold"))
+caption_label.grid(row=0, column=1)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
